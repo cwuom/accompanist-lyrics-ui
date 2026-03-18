e5406e9370d3294ba5959f58ec888705467c33ba
Simon Scholz <Simon.Sleeeep@outlook.com>
2026-03-18 22:44:06 +0800

    chore: Update sign configs

--- sample/build.gradle.kts
+++ sample/build.gradle.kts
@@ -80,19 +80,23 @@ android {
     }
 
     signingConfigs {
-        val signingProps = Properties().apply {
-            val file = rootProject.file("signing.properties")
-            if (file.exists()) {
-                file.inputStream().use { load(it) }
+        val sFile = project.findProperty("RELEASE_STORE_FILE") as? String
+        val sPassword = project.findProperty("RELEASE_STORE_PASSWORD") as? String
+        val kAlias = project.findProperty("RELEASE_KEY_ALIAS") as? String
+        val kPassword = project.findProperty("RELEASE_KEY_PASSWORD") as? String
+
+        if (sFile != null && sPassword != null && kAlias != null && kPassword != null) {
+            create("release") {
+                storeFile = file(sFile)
+                storePassword = sPassword
+                keyAlias = kAlias
+                keyPassword = kPassword
+
+                enableV1Signing = true
+                enableV2Signing = true
+                enableV3Signing = true
+                enableV4Signing = true
             }
-        }
-        create("release") {
-            storeFile = signingProps["RELEASE_STORE_FILE"]?.let { rootProject.file(it as String) }
-            storePassword = signingProps["RELEASE_STORE_PASSWORD"] as String?
-            keyAlias = signingProps["RELEASE_KEY_ALIAS"] as String?
-            keyPassword = signingProps["RELEASE_KEY_PASSWORD"] as String?
-            enableV1Signing = true
-            enableV2Signing = true
         }
     }
 
